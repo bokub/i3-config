ca1ed10bf575b3a29875b0499279295462eee34d
Boris Kubiak <kubiakboris@gmail.com>
2020-03-30 14:39:16 +0200

    Edit i3blocks icons, tweak some stuff

--- README.md
+++ README.md
@@ -16,8 +16,10 @@ All these files should be placed in `~/.config/i3`
 - [i3blocks](https://github.com/vivien/i3blocks) (status bar)
 - [Rofi](https://github.com/DaveDavenport/rofi) (app launcher)
 - [Compton](https://github.com/chjj/compton) (shadows & opacity)
+- [Dunst](https://github.com/dunst-project/dunst) (notification daemon)
 - [Font Awesome](http://fontawesome.io/) (icons)
 - [Nitrogen](https://github.com/l3ib/nitrogen/) (wallpaper manager)
+- [Starship](https://github.com/starship/starship) (shell prompt)
 - [Snazzy](https://github.com/tobark/hyper-snazzy-gnome-terminal) (terminal color scheme)
 
 ## Quick install
@@ -29,12 +31,13 @@ curl -fsSL https://git.io/i3-config | bash
 
 The command above will:
 
-- Install i3-gaps dependencies
-- Build i3-gaps from source
-- Install i3blocks, Rofi, Compton, Font Awesome and Nitrogen
+- Install i3-gaps dependencies and build i3-gaps from source
+- Install i3blocks, Rofi, Compton, Dunst and Nitrogen
+- Download and install Font Awesome fonts
 - Override i3 config with this repository
 - Download and set the wallpaper
+- Install Starship
 - Install Snazzy color scheme for gnome-terminal
 
-It should work on a fresh Ubuntu setup. For any other distro, I have no idea.
+The script should work on a fresh Ubuntu setup. For any other distro, I have no idea.
 
--- quick-install.sh
+++ quick-install.sh
@@ -21,18 +21,34 @@ rm -rf build/ && mkdir -p build && cd build/
 make && sudo make install
 
 # Install other tools
-sudo apt install -y i3blocks rofi compton fonts-font-awesome nitrogen xdotool
+sudo apt install -y i3blocks rofi compton nitrogen xdotool dunst
+
+# Download and install font-awesome
+sudo mkdir -p /usr/share/fonts/opentype/font-awesome
+sudo wget https://git.io/JvHi9 -O '/usr/share/fonts/opentype/font-awesome/Font Awesome 5 Free-Solid-900.otf'
 
 # Override config
 mkdir -p ~/.config && cd ~/.config && rm -fr i3
 git clone https://github.com/bokub/i3-config.git i3
 
-# Download wallpaper
-cd && mkdir -p wallpapers && wget https://github.com/bokub/i3-config/blob/images/wallpapers/blue_pink_gradient.jpg -O wallpapers/blue_pink_gradient.jpg
+# Download and set wallpaper
+mkdir -p ~/wallpapers
+wget https://git.io/Jv7wA -O ~/wallpapers/blue_pink_gradient.jpg
+nitrogen --save --set-scaled ~/wallpapers/blue_pink_gradient.jpg --head=0 && sleep 0.5
+nitrogen --save --set-scaled ~/wallpapers/blue_pink_gradient.jpg --head=1
+
+# Install Starship
+cd
+wget https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz
+sudo tar -xzf starship-x86_64-unknown-linux-gnu.tar.gz -C /usr/local/bin
+rm starship-x86_64-unknown-linux-gnu.tar.gz
+echo 'eval "$(starship init bash)"' >> .bashrc
+
+curl -fsSL https://starship.rs/install.sh | bash -f
 
 # Install Snazzy theme for gnome-terminal
 cd && git clone https://github.com/tobark/hyper-snazzy-gnome-terminal.git snazzy && cd snazzy
 chmod +x hyper-snazzy.sh && ./hyper-snazzy.sh
 
 # Cleanup
-cd && rm -fr i3-gaps snazzy
+cd && rm -fr i3-gaps snazzy Font-Awesome
