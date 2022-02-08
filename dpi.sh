f5019d759dd4e0cac530e01b867121ae9a786253
Boris Kubiak <kubiakboris@gmail.com>
2022-02-08 15:25:07 +0100

    Improve DPI script

--- dpi.sh
+++ dpi.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+#
+# Quickly change HiDpi settings to 'high' or 'low'
+# N.B: You need to log out and log in again to see the changes.
+#  
+# Usage:
+# $ dpi.sh high|low
+#
+
 process_arguments() {
     if [ $# -eq 0 ]; then
-        echo "No arguments provided. First argument should be 'high' or 'low'"
+        echo "Usage: dpi.sh high|low"
         exit 1
     fi
     while [ -n "$1" ]
@@ -10,27 +18,27 @@ process_arguments() {
         case $1 in
             high) hidpi; shift; break;;
             low) lowdpi; shift; break;;
-            *) echo "First argument should be 'high' or 'low'"; exit 1;;
+            *) echo "Usage: dpi.sh high|low"; exit 1;;
         esac
     done
 }
 
 hidpi() {
-    echo "Xft.dpi: 125 # High
-# Xft.dpi: 96 # Low" > ~/.Xresources
+    # Write Xft.dpi in .Xresources
+    echo -e "Xft.dpi: 125 # High\n# Xft.dpi: 96 # Low" > ~/.Xresources
 
+    # Replace GDK_DPI_SCALE in .profile
     sed -i '/GDK_DPI_SCALE/d' ~/.profile
-    echo "export GDK_DPI_SCALE=1.25 # High" >> ~/.profile
-    echo "# export GDK_DPI_SCALE=1 # Low" >> ~/.profile
+    echo -e "export GDK_DPI_SCALE=1.25 # High\n# export GDK_DPI_SCALE=1 # Low" >> ~/.profile
 }
 
 lowdpi() {
-    echo "Xft.dpi: 96 # Low
-# Xft.dpi: 125 # High" > ~/.Xresources
+    # Write Xft.dpi in .Xresources
+    echo -e "Xft.dpi: 96 # Low\n# Xft.dpi: 125 # High" > ~/.Xresources
 
+    # Replace GDK_DPI_SCALE in .profile
     sed -i '/GDK_DPI_SCALE/d' ~/.profile
-    echo "export GDK_DPI_SCALE=1 # Low" >> ~/.profile
-    echo "# export GDK_DPI_SCALE=1.25 # High" >> ~/.profile
+    echo -e "export GDK_DPI_SCALE=1 # Low\n# export GDK_DPI_SCALE=1.25 # High" >> ~/.profile
 }
 
 process_arguments "$@"
