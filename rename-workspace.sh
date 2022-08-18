d93654a653c18921b368ed7f91d327cccc843732
Boris Kubiak <kubiakboris@gmail.com>
2022-08-18 10:13:50 +0200

    Minor fixes and improvements
    - Improve workspace names
    - Fix flameshot commands
    - Auto switch audio source
    - Remove broken script

--- rename-workspace.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-OLD=$(i3-msg -t 'get_workspaces' | sed -e 's/{"num/\n{"num/g' | grep \"focused\":true | sed -e 's/,"/\n/g' | grep name | cut -d\" -f 3)
-NEW=$(zenity --text="Enter new name:" --entry --title="Rename workspace $OLD" --entry-text="$OLD : ")
-echo "rename workspace \"$OLD\" to \"$NEW\""
-i3-msg "rename workspace \"$OLD\" to \"$NEW\""
